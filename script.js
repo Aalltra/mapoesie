9f0f4ddcffe53421a0521abaa1e6a9d577869746
Aalltrac <aalltrac@gmail.com>
2025-02-06 16:52:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Toggle highlight on click for each poem paragraph
   const poemParagraphs = document.querySelectorAll('.poem p');
   
   poemParagraphs.forEach(p => {
